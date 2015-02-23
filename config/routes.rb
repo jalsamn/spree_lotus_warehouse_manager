34a581c4ce21a9f9d04120c392b4cc97ab771a48
Gunjan Patel <gpatel@janakfoods.com>
2015-02-23 01:58:10 +0000

    s

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Spree::Core::Engine.routes.draw do
   
   namespace :admin do
     resources :vendors
+    get 'vendors/assignvendor', :to => 'vendors#assignvendor'
+
   end
 
   # Add your extension routes here
