727ed925b90ed505f841b8658195fba2984cc4ea
Gunjan Patel <gpatel@janakfoods.com>
2015-03-01 20:57:30 +0000

    s

--- app/controllers/spree/admin/vendors_controller.rb
+++ app/controllers/spree/admin/vendors_controller.rb
@@ -12,6 +12,10 @@ class VendorsController < Spree::Admin::BaseController
   def assignvendor
    # @vars = Spree::Variant.where(vendor_id: nil)
     @vars = Spree::Variant.where(vendor_id: nil).order(:barcode).page params[:page]
+    respond_to do |format|
+     # format.html { redirect_to tasks_url }
+      
+    end
   end
   
   def updatevendor
@@ -20,7 +24,7 @@ class VendorsController < Spree::Admin::BaseController
     respond_to do |format|
         if @v.save
           format.html { redirect_to admin_assignvendor_path, notice: 'Updated' }
-           # format.js
+           format.js
         else
             format.html { render action: "updatevendor" }
         end
