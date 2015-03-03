d604c8f2bab8d7133d58cf3eeb2071e3a911dd21
Gunjan Patel <gpatel@janakfoods.com>
2015-03-03 22:03:19 +0000

    s

--- app/assets/javascripts/spree/backend/spree_lotus_warehouse_manager.js
+++ app/assets/javascripts/spree/backend/spree_lotus_warehouse_manager.js
@@ -1,12 +1,3 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
 
-
-$("#variant_chk").bind('change', function(){
-  if (this.checked){
-    alert("yes checked");
-  }
-  else {
-     alert("no");
-  }
-});
