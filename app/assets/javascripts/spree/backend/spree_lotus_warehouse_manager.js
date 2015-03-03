cc45dabc102ef729f0aeca2228ae39a3a82c5343
Gunjan Patel <gpatel@janakfoods.com>
2015-03-03 21:46:08 +0000

    s

--- app/assets/javascripts/spree/backend/spree_lotus_warehouse_manager.js
+++ app/assets/javascripts/spree/backend/spree_lotus_warehouse_manager.js
@@ -2,7 +2,7 @@
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
 
 
-$(".task-check").bind('change', function(){
+$("#variant_chk").bind('change', function(){
   if (this.checked){
     alert("yes checked");
   }
